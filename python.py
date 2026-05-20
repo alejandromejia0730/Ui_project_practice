7bc368ea40eeddcd8059acbf690962c12007d307
alejandromejia0730 <154280648+alejandromejia0730@users.noreply.github.com>
2026-05-20 10:54:04 -0500

    Scraping

---
+++ python.py
@@ -0,0 +1,53 @@
+import requests as rq
+from bs4 import BeautifulSoup
+from datetime import datetime
+import json
+
+url = "http://127.0.0.1:5500/Save.html"
+page = rq.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+tittles = soup.find_all('h4',class_='a-header--4')
+resultados = []
+
+for t in tittles : 
+    tittle = t.get_text(strip=True)
+
+    table = t.find_next_sibling('table')
+
+    img = table.select_one('img')
+
+    img_url = img.get("data-src")
+
+    td = table.select_one('td.center')
+    duration = td.get_text(strip=True).split(' - ')
+
+    init = datetime.strptime(duration[0], "%B %d, %Y")
+    if duration[1] != "Permanent":
+        finish = datetime.strptime(duration[1], "%B %d, %Y")
+    else :
+        finish = None
+
+    p = t.find_next_sibling('p')
+    desc = p.get_text(strip=True)
+
+    a = p.find_next_sibling('p').select_one('a.a-btn')
+    if a:
+        more_info = a['href']
+    else :
+        more_info = ""   
+
+
+    resultados.append({
+        'tittle' : tittle,
+        'desc' :  desc,
+        'more_info': more_info,
+        'init' : str(init.date()),
+        'finish' : str(finish.date()) if finish else None,
+        'img' : img_url
+    })
+
+print(json.dumps(resultados))
+
+
+
+
